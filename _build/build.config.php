6bd0d263a6399526c1173e4abd35cfe928b8e083
Bezumkin <bezumkin@yandex.ru>
2013-10-03 10:20:37 +0700

    Improved builder

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,11 +1,23 @@
 <?php
-/**
- * Define the MODX path constants necessary for installation
- *
- * @package themebootstrap
- * @subpackage build
- */
-define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+
+/* define package */
+define('PKG_NAME','Theme.Bootstrap');
+define('PKG_NAME_LOWER','themebootstrap');
+
+define('PKG_VERSION','1.3.1');
+define('PKG_RELEASE','pl');
+define('PKG_AUTO_INSTALL', true);
+
+/* define paths */
+if (isset($_SERVER['MODX_BASE_PATH'])) {
+	define('MODX_BASE_PATH', $_SERVER['MODX_BASE_PATH']);
+}
+elseif (is_file(dirname(dirname(dirname(__FILE__))) . '/index.php' )) {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+}
+else {
+	define('MODX_BASE_PATH', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
+}
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
 define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'connectors/');
--- _build/build.transport.php
+++ _build/build.transport.php
@@ -2,7 +2,7 @@
 /**
  * Theme.Bootstrap build script
  *
- * @package themebootstrap 
+ * @package themebootstrap
  * @subpackage build
  */
 $mtime = microtime();
@@ -11,27 +11,22 @@ $mtime = $mtime[1] + $mtime[0];
 $tstart = $mtime;
 set_time_limit(0);
 
-/* define package */
-define('PKG_NAME','Theme.Bootstrap');
-define('PKG_NAME_LOWER','themebootstrap');
-define('PKG_VERSION','1.3.1');
-define('PKG_RELEASE','pl');
+require_once 'build.config.php';
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
 $sources = array(
-    'root' => $root,
-    'build' => $root . '_build/',
-    'data' => $root . '_build/data/',
-    'chunks' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/chunks/',
-    'templates' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/templates/',
-    'docs' => $root.'assets/components/'.PKG_NAME_LOWER.'/docs/',
-    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
+	'root' => $root,
+	'build' => $root . '_build/',
+	'data' => $root . '_build/data/',
+	'chunks' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/chunks/',
+	'templates' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/templates/',
+	'docs' => $root.'assets/components/'.PKG_NAME_LOWER.'/docs/',
+	'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
 );
 unset($root);
 
 /* override with your own defines here (see build.config.sample.php) */
-require_once $sources['build'] . '/build.config.php';
 require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
 require_once $sources['build'] . '/includes/functions.php';
 
@@ -55,76 +50,76 @@ $category->set('category',PKG_NAME);
 /* add templates */
 $templates = include $sources['data'].'transport.templates.php';
 if (!is_array($templates)) {
-    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in templates.');
+	$modx->log(modX::LOG_LEVEL_ERROR,'Could not package in templates.');
 } else {
-    $category->addMany($templates);
-    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($templates).' templates.');
+	$category->addMany($templates);
+	$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($templates).' templates.');
 }
 
 /* add chunks */
 $chunks = include $sources['data'].'transport.chunks.php';
 if (!is_array($chunks)) {
-    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in chunks.');
+	$modx->log(modX::LOG_LEVEL_ERROR,'Could not package in chunks.');
 } else {
-    $category->addMany($chunks);
-    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($chunks).' chunks.');
+	$category->addMany($chunks);
+	$modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($chunks).' chunks.');
 }
 
 
 /* create category vehicle */
 $attr = array(
-    xPDOTransport::UNIQUE_KEY => 'category',
-    xPDOTransport::PRESERVE_KEYS => false,
-    xPDOTransport::UPDATE_OBJECT => true,
-    xPDOTransport::RELATED_OBJECTS => true,
-    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-        'Children' => array(
-            xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => true,
-            xPDOTransport::UNIQUE_KEY => 'category',
-            xPDOTransport::RELATED_OBJECTS => true,
-            xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-                'Templates' => array(
-                    xPDOTransport::PRESERVE_KEYS => false,
-                    xPDOTransport::UPDATE_OBJECT => false,
-                    xPDOTransport::UNIQUE_KEY => 'templatename',
-                ),
-                'Chunks' => array(
-                    xPDOTransport::PRESERVE_KEYS => false,
-                    xPDOTransport::UPDATE_OBJECT => false,
-                    xPDOTransport::UNIQUE_KEY => 'name',
-                ),
-            ),
-        ),
-        'Templates' => array(
-            xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => false,
-            xPDOTransport::UNIQUE_KEY => 'templatename',
-        ),
-        'Chunks' => array (
-            xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => false,
-            xPDOTransport::UNIQUE_KEY => 'name',
-        ),
-    ),
+	xPDOTransport::UNIQUE_KEY => 'category',
+	xPDOTransport::PRESERVE_KEYS => false,
+	xPDOTransport::UPDATE_OBJECT => true,
+	xPDOTransport::RELATED_OBJECTS => true,
+	xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
+		'Children' => array(
+			xPDOTransport::PRESERVE_KEYS => false,
+			xPDOTransport::UPDATE_OBJECT => true,
+			xPDOTransport::UNIQUE_KEY => 'category',
+			xPDOTransport::RELATED_OBJECTS => true,
+			xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
+				'Templates' => array(
+					xPDOTransport::PRESERVE_KEYS => false,
+					xPDOTransport::UPDATE_OBJECT => false,
+					xPDOTransport::UNIQUE_KEY => 'templatename',
+				),
+				'Chunks' => array(
+					xPDOTransport::PRESERVE_KEYS => false,
+					xPDOTransport::UPDATE_OBJECT => false,
+					xPDOTransport::UNIQUE_KEY => 'name',
+				),
+			),
+		),
+		'Templates' => array(
+			xPDOTransport::PRESERVE_KEYS => false,
+			xPDOTransport::UPDATE_OBJECT => false,
+			xPDOTransport::UNIQUE_KEY => 'templatename',
+		),
+		'Chunks' => array (
+			xPDOTransport::PRESERVE_KEYS => false,
+			xPDOTransport::UPDATE_OBJECT => false,
+			xPDOTransport::UNIQUE_KEY => 'name',
+		),
+	),
 );
 $vehicle = $builder->createVehicle($category,$attr);
 
 $modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to category...');
 $vehicle->resolve('file',array(
-    'source' => $sources['source_assets'],
-    'target' => "return MODX_ASSETS_PATH . 'components/';",
+	'source' => $sources['source_assets'],
+	'target' => "return MODX_ASSETS_PATH . 'components/';",
 ));
 $builder->putVehicle($vehicle);
 
 /* now pack in the license file, readme and setup options */
 $builder->setPackageAttributes(array(
-    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
-    'license' => file_get_contents($sources['docs'] . 'license.txt'),
-    'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
-    //'setup-options' => array(
-        //'source' => $sources['build'].'setup.options.php',
-    //),
+	'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+	'license' => file_get_contents($sources['docs'] . 'license.txt'),
+	'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
+	//'setup-options' => array(
+		//'source' => $sources['build'].'setup.options.php',
+	//),
 ));
 $modx->log(modX::LOG_LEVEL_INFO,'Added package attributes and setup options.');
 
@@ -139,6 +134,40 @@ $tend= $mtime;
 $totalTime= ($tend - $tstart);
 $totalTime= sprintf("%2.4f s", $totalTime);
 
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+if (defined('PKG_AUTO_INSTALL') && PKG_AUTO_INSTALL) {
+	$signature = $builder->getSignature();
+	$sig = explode('-',$signature);
+	$versionSignature = explode('.',$sig[1]);
 
-exit ();
+	/* @var modTransportPackage $package */
+	if (!$package = $modx->getObject('transport.modTransportPackage', array('signature' => $signature))) {
+		$package = $modx->newObject('transport.modTransportPackage');
+		$package->set('signature', $signature);
+		$package->fromArray(array(
+			'created' => date('Y-m-d h:i:s'),
+			'updated' => null,
+			'state' => 1,
+			'workspace' => 1,
+			'provider' => 0,
+			'source' => $signature.'.transport.zip',
+			'package_name' => $sig[0],
+			'version_major' => $versionSignature[0],
+			'version_minor' => !empty($versionSignature[1]) ? $versionSignature[1] : 0,
+			'version_patch' => !empty($versionSignature[2]) ? $versionSignature[2] : 0,
+		));
+		if (!empty($sig[2])) {
+			$r = preg_split('/([0-9]+)/',$sig[2],-1,PREG_SPLIT_DELIM_CAPTURE);
+			if (is_array($r) && !empty($r)) {
+				$package->set('release',$r[0]);
+				$package->set('release_index',(isset($r[1]) ? $r[1] : '0'));
+			} else {
+				$package->set('release',$sig[2]);
+			}
+		}
+		$package->save();
+	}
+	$package->install();
+}
+
+$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Execution time: {$totalTime}\n");
+echo '</pre>';
