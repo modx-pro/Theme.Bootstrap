e0a00d974c47ecb4040ff425ea2f3e18246ba022
Bezumkin <bezumkin@yandex.ru>
2013-11-23 00:25:56 +0700

    Version 2.0.2-pl1

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME','Theme.Bootstrap');
 define('PKG_NAME_LOWER','themebootstrap');
 
 define('PKG_VERSION','2.0.2');
-define('PKG_RELEASE','pl');
+define('PKG_RELEASE','pl1');
 define('PKG_AUTO_INSTALL', true);
 
 /* define paths */
